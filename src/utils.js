a810ff53768fa658b345f2aa4a7dee658e67184c
nikoloz-code23 <literallynickk@gmial.com>
2025-06-10 19:24:10 +0400

    init

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+function powerOfTwo(number) {
+    return number**2;
+}
+
+module.exports = powerOfTwo;
---
+++ src/utils.test.js
@@ -0,0 +1,5 @@
+const powerOfTwo = require('./utils');
+
+test('raise 2 by the power of Two', () => {
+    expect(powerOfTwo(2)).toBe(4);
+});
